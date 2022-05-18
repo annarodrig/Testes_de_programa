1fd40df4f5636cb3365f6975e1078d49f9cb0e7f
Anna Beatriz <annab_oliver@hotmail.com>
2022-05-18 02:17:34 -0300

    Finaliza melhorias

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿int opcao;
+double numero;
+double resultado;
+
+do
+{
+    Console.WriteLine("SELECIONE UMA OPÇÃO");
+    Console.WriteLine("1- CALCULAR O DOBRO DO NÚMERO");
+    Console.WriteLine("2- CALCULAR A TABUADA DO NÚMERO");
+    Console.WriteLine("0- SAIR");
+
+    opcao = Convert.ToInt32(Console.ReadLine());
+    if (opcao == 1)
+    {
+        Console.Clear();
+        Console.Write("Insira um número:");
+        numero = Convert.ToDouble(Console.ReadLine());
+        Console.Clear();
+        resultado = numero * 2;
+        Console.WriteLine($"O resultado foi:{resultado} ");
+        
+    }
+    else if (opcao == 2)
+    {
+        Console.Clear();
+        Console.Write("Insira um número:");
+        numero = Convert.ToDouble(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++)
+        {
+            resultado = numero * i;
+            Console.WriteLine(resultado);
+        }
+    }
+
+} while (opcao != 0);
+Environment.Exit(0);
